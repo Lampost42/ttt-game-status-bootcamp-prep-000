aa5fc34690f2128f9f225d8494f7deeca60d3634
Trevor Russell <truss.42@gmail.com>
2018-05-22 21:15:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ WIN_COMBINATIONS = [
   end
   
   def full?(board)
-    if (board.all?{|space| board[space] == "X" || board[space] == "O"})
+    if (board.all? {|space| board[space] == "X" || board[space] == "O"})
       return TRUE
     else
       return FALSE
